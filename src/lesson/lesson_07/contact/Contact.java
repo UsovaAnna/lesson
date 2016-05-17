42a4c77db7db8bbc16b29599997122d43973d3d8
usova <usova@improveitgroup.com>
2016-05-17 21:52:50 +0600

    lesson 7

---
+++ src/lesson/lesson_07/contact/Contact.java
@@ -0,0 +1,34 @@
+package lesson.lesson_07.contact;
+
+/**
+ * Created by admin on 17.05.2016.
+ */
+public class Contact {
+    private String lastName;
+    private String firstName;
+    private String mobile;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
